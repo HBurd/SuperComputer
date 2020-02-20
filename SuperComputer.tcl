dbb0d94289f31b584582b36e2eed24712f9c99fa
Hugo Burd <hburd@uvic.ca>
2020-02-20 13:57:42 -0800

    added execute tb to tcl script

--- SuperComputer.tcl
+++ SuperComputer.tcl
@@ -211,6 +211,7 @@ set files [list \
  "[file normalize "$origin_dir/sim/RegFile_TB.vhd"]"\
  "[file normalize "$origin_dir/sim/alu_tb.vhd"]"\
  "[file normalize "$origin_dir/sim/decode_tb.vhd"]"\
+ "[file normalize "$origin_dir/sim/execute_tb.vhd"]"\
 ]
 add_files -norecurse -fileset $obj $files
 
