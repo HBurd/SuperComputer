ccd3704ee1712049c9c28eda24946c9e7eb1ec9a
Hugo Burd <hburd@uvic.ca>
2020-02-20 17:13:42 -0800

    updated tcl script for writeback stage

--- SuperComputer.tcl
+++ SuperComputer.tcl
@@ -144,6 +144,7 @@ set files [list \
  "[file normalize "$origin_dir/hdl/mmu.vhd"]"\
  "[file normalize "$origin_dir/hdl/DecodeStage.vhd"]"\
  "[file normalize "$origin_dir/hdl/ExecuteStage.vhd"]"\
+ "[file normalize "$origin_dir/hdl/WriteBack.vhd"]"\
 ]
 add_files -norecurse -fileset $obj $files
 
