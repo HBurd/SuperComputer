f696e4a2a3fc8ada802c18b09468d50972f08f2d
Jeff <jeffstacey111@gmail.com>
2020-03-02 20:04:45 -0800

    Add bubbles for ALU instructions too

--- hdl/DecodeStage.vhd
+++ hdl/DecodeStage.vhd
@@ -96,6 +96,9 @@ instr_fmt <=
 insert_bubble <= '1' when (
     (instr_fmt = fmt_l2 and ((ridx1_pending = '1') or (ridx2_pending = '1'))) or 
     (instr_fmt = fmt_l1 and (ridx1_pending = '1')) or
+    (instr_fmt = fmt_a1 and ((ridx1_pending = '1') or (ridx2_pending = '1'))) or
+    (instr_fmt = fmt_a2 and (ridx1_pending = '1')) or
+    (instr_fmt = fmt_a3 and (ridx1_pending = '1')) or
     (instr_fmt = fmt_b2 and (ridx1_pending = '1')))
     else '0';
 bubble <= insert_bubble;
@@ -105,11 +108,11 @@ opcode <= op_nop when insert_bubble = '1' else opcode_internal;
 mark_pending <= '1' when (insert_bubble = '0') and (instr_fmt = fmt_a1 or instr_fmt = fmt_a2 or instr_fmt = fmt_a3 or instr_fmt = fmt_b2 or instr_fmt = fmt_l2 or instr_fmt = fmt_l1) 
     else '0';
     
-write_idx <= unsigned(instr(8 downto 6)) when (instr_fmt = fmt_a1 or instr_fmt = fmt_a2 or instr_fmt = fmt_a3 or instr_fmt = fmt_b2 or instr_fmt = fmt_l2)
+write_idx <= unsigned(instr(8 downto 6)) when (instr_fmt = fmt_a1 or instr_fmt = fmt_a2 or instr_fmt = fmt_b2 or instr_fmt = fmt_l2)
     else "111" when instr_fmt = fmt_l1
     else (others => '0');
     
-read_idx_1 <= unsigned(instr(5 downto 3)) when (instr_fmt = fmt_a1 or instr_fmt = fmt_l2)
+read_idx_1 <= unsigned(instr(5 downto 3)) when (instr_fmt = fmt_a1 or instr_fmt = fmt_a3 or instr_fmt = fmt_l2)
     else unsigned(instr(8 downto 6)) when (instr_fmt = fmt_a4 or instr_fmt = fmt_a2)
     else "111" when instr_fmt = fmt_l1  -- loadimm loads into r7
     else (others => '0');
